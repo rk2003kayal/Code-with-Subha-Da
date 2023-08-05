f3c5223c190508116c225eb50b7e067c44dee9dd
Rounak Kayal <96925350+rk2003kayal@users.noreply.github.com>
2023-08-05 14:11:13 +0530

    Create StarPatterP.java

---
+++ Rounak Kayal/StarPatterP.java
@@ -0,0 +1,24 @@
+// Print a star pattern square
+
+import java.util.*;
+
+public class StarPatterP
+{
+    public static void main(String[] args)
+    {
+        Scanner ip = new Scanner(System.in);
+
+        System.out.println("Enter n : ");
+        int n = ip.nextInt();
+        int m = n;
+
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
